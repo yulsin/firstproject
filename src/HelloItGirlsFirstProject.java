af6851626a6c253c9e10eda57b2c9b17618fbea3
ksenkl <klepa@vivaldi.net>
2023-05-23 15:12:36 +0300

    first commit

--- src/HelloItGirlsFirstProject.java
+++ src/HelloItGirlsFirstProject.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class HelloItGirlsFirstProject {
+public class HelloItGirlsFirstProject {
+    public static void main(String[] args) {
+        System.out.println("Hello It Girls!");
+    }
 }
